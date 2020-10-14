b0291b1566f71e62d4f8e2fc28cdf0025488f2ed
Gabrie2121 <gabriel.miranda041200@gmail.com>
2020-10-14 20:46:35 -0300

    Iniciar a logica

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 import './Button.css'
 
 export default props=>{
+    //pode fazer isso das classes aqui em cima usando variaveis com +=
+    //EX:
+    //let classes = 'button'
+    //classes+= props.operation ? 'operation' : ''
     return(
-        <button className='button'>{props.label}</button>
+        <button className={`
+                button
+                ${props.operation ? 'operation' : ''}
+                ${props.double ? 'double' : ''}
+                ${props.triple ? 'triple' : ''}
+        `}onClick={e=>props.click && props.click(props.label)}>{props.label}</button>
     )
 }

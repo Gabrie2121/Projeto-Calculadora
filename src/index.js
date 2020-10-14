a816a83d22a156b1c99bf2276744af31240025e6
Gabrie2121 <gabriel.miranda041200@gmail.com>
2020-10-14 20:55:44 -0300

    Lokura

--- src/index.js
+++ src/index.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Calculator from './main/Calculator';
+import App from './App'
 import * as serviceWorker from './serviceWorker';
 
 const el = document.getElementById('root');
 
 ReactDOM.render(
   <React.Fragment>
-    <h1>Calculadora</h1>
-    <Calculator />
+    <App></App>
   </React.Fragment>,
   el
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
